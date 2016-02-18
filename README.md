b29b794362271b87e18839053a1e1f99a644bfbb
Chris Canipe <chris.canipe@wsj.com>
2016-02-17 22:10:13 -0600

    documented the script.js file and added a README file

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-web-app-template-simple
-=======================
+#### Exercise Three: Looping through data to create a simple bar chart.
 
-A simple web app template with jQuery, and Bootstrap.  | Mizzou Workshop - Oct. 2014
+We loaded up our Columbia Public Schools JSON in class Tuesday and iterated through the rows using a "for loop." We touched on three main concepts:
+
+1) Loading data via JQuery's [super simple ajax method, $.getJSON](http://www.w3schools.com/jquery/ajax_getjson.asp):
+
+```
+$.getJSON("js/filename.json", function(data) {
+	doSomething(data);
+})
+```
+
+2) Passed the data between functions. (See [Paramaters and Arguments](http://www.w3schools.com/js/js_function_parameters.asp)
+
+3) We looped through our data, and appended some markup to the page to create a simple bar chart. Some references you might find useful:
+* [For loops](http://www.w3schools.com/js/js_loop_for.asp)
+* [Variables](http://www.w3schools.com/js/js_variables.asp)
+* [Operators](http://www.w3schools.com/js/js_operators.asp)
--- js/script.js
+++ js/script.js
@@ -1,37 +1,44 @@
+
+//Nothing happens on the page until the document is loaded.
+//Once loaded, we call the getData() function, defined below.
 $(document).ready(function() {
 	getData();
 });
 
 
+function getData() {	
 
-function getData() {
-	
+	//We bring our json file into our JavaScript environement with an AJAX call.
+	//$.getJSON points to our json file, loads it, and stores it in a variable defined in the parenthesis...
+	//In the case, we've opted to call it `schoolsData`
 	$.getJSON("js/columbia.json", function(schoolsData) {
-		//Callback runs after the data is loaded.
+		//Once we have the data, we PASS it on to the next function
+		//where we'll loop through the data and draw our chart.
+		//We pass it to the next function by including it as an "argument" inside the parenthesis
 		loopThroughData(schoolsData);
 	});
-
-
-
 }
 
 
 
+//This is the function we'll use to draw our chart using the schools data.
+//In order to use it in the function, we need to "catch" the data we passed from the other function
+//by putting it in the parenthesis. In class we called is "s" to make the point that...
+//...we can call it anything we want, but it's value will be the same as from where we passed it.
+//I'm keeping the name "schoolsData" here for clarity.
+function loopThroughData(schoolsData) {
 
 
-function addTwoNumbers(num1, num2) {
-	console.log(num1 + num2);
-}
+	// The for loop:
+	// 1) We define a variable called i, which is equal to 0.
+	// 2) If i is less than the length of the data.movies array, proceed.
+	// 3) Add 1 to i. (i++ is shorthand for i = i + 1)
 
+	for (i=0; i<schoolsData.length; i++) {
 
-
-function loopThroughData(s) {
-	
-	for (i=0; i<s.length; i++) {
-
-		var schoolName = s[i]["SCHOOL_NAME"];
-		var year = s[i]["YEAR"];
-		var gradPct = s[i]["GRADUATES_PCT"];
+		var schoolName = schoolsData[i]["SCHOOL_NAME"];
+		var year = schoolsData[i]["YEAR"];
+		var gradPct = schoolsData[i]["GRADUATES_PCT"];
 
 		//If graduation percent is less thatn 50%...
 		//We use the `<` operator to test for values less than 50%
@@ -44,10 +51,20 @@ function loopThroughData(s) {
 
 		var barWidth = gradPct * 10;
 
+		//If the school name matches the one we're looking for, add some markup onto our page.
 		if (schoolName === "FREDERICK DOUGLASS HIGH") {
+
+			//All I'm doing here is writing some markup. The markup is exactly the same each time...
+			//...except for three variables: the width of the bar (calculated above), the year and the graduation rate.
+			//So we fill those in each time through the loop.
 			$(".chart").append(
-				"<div class='bar' style='width: "+barWidth+"px'></div>"
+				"<div class='bar-item'>"+
+					"<div class='bar' style='width: "+barWidth+"px'>"+year+"</div>"+
+					"<div class='val'>"+gradPct+"%</div>"+
+				"</div>"	
 			);
+
+			//See the style.css document for CSS to make it pretty.
 		}
 
 
